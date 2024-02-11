82b147b01756ee469666786e425da661d2d69801
AlexandreRCS <ale.maresias@gmail.com>
2024-02-11 20:22:39 -0300

    update
    Inicio do JS

--- js/favorites.js
+++ js/favorites.js
@@ -2,4 +2,25 @@
 
 //COMO OS DADOS SERÃO ESTRUTURADOS
 
+export class Favorites{
+    constructor(RecebendoEstrutura){
+        this.estrutura =  document.querySelector(RecebendoEstrutura)
+
+    }
+}
+
+export class FavoritesView extends Favorites{
+    constructor(manipulandoEstrutura){
+        super(manipulandoEstrutura)
+
+        this.removeoffFavorites()
+    }
+
+    removeoffFavorites(){
+        const teste = this.estrutura.querySelector('#wrapper-input')
+        console.log(teste)
+    }
+}
+
+
 //CLASSE QUE VAI CRIAR A VISUALIZAÇÃO E EVENTO HTML
