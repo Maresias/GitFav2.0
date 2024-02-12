146e8ee4e848d24841b8ec61d79257c44174bd4e
AlexandreRCS <ale.maresias@gmail.com>
2024-02-11 21:29:58 -0300

    update
    update JavaScript

--- js/favorites.js
+++ js/favorites.js
@@ -3,23 +3,42 @@
 //COMO OS DADOS SERÃO ESTRUTURADOS
 
 export class Favorites{
-    constructor(RecebendoEstrutura){
-        this.estrutura =  document.querySelector(RecebendoEstrutura)
+    constructor(RecebendoEstruturaDoFavoritesView){
+        this.estrutura =  document.querySelector(RecebendoEstruturaDoFavoritesView)
 
     }
 }
 
 export class FavoritesView extends Favorites{
-    constructor(manipulandoEstrutura){
-        super(manipulandoEstrutura)
-
-        this.removeoffFavorites()
+    constructor(RecebendoEstruturaPorAqui){
+        super(RecebendoEstruturaPorAqui)
+        console.log(this.estrutura)
     }
 
     removeoffFavorites(){
-        const teste = this.estrutura.querySelector('#wrapper-input')
-        console.log(teste)
+        const span = this.estrutura.querySelector('main span')
+        const estrela = this.estrutura.querySelector('main table')
+
+        span.textContent = ""
+        estrela.classList.remove('off-favorites')
     }
+
+    createRow(){
+        const tr = document.createElement('tr')
+        
+        const content = 
+        `<td class="user">
+            <img src="https://github.com/alexandrercs.png" alt="">
+            <a href="https://github.com/alexandrercs" target="_blank" rel="noopener noreferrer">
+                <p>Alexandre</p>
+                <span>alexandrercs</span>
+            </a>
+        </td>
+        <td class="repositories">37</td>
+        <td class="followes">137</td>
+        <td><button id="remover">Remover</button></td>`
+    }
+
 }
 
 
